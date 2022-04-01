cf41fab7a9f8149db151a8ac7fb223a29949c07d
Simodalstix <lastpolar@gmail.com>
2022-04-01 14:42:49 +1100

    testing .env

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+require('dotenv').config();
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Layout/Navbar'
 import Footer from './components/Layout/Footer'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Notfound from './Pages/Notfound'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/notfound' element={<Notfound />}/>
-          <Route path='/*' element={<Notfound />}/>
-        </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<Notfound />} />
+              <Route path='/*' element={<Notfound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
