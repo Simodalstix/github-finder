19795c21b70705da19b1a416aeaa390a565adcb9
Simodalstix <lastpolar@gmail.com>
2022-04-01 23:16:50 +1100

    removed useEffect and set up reducer

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import { useState, createContext } from 'react'
+import { useReducer, createContext } from 'react'
+import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
@@ -6,10 +7,18 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  const initialState = {
+    users: [],
+    loading: false,
+  }
 
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  // Get initial users (testing purposes)
+  // Want to set up search users
   const fetchUsers = async () => {
+    setLoading()
+
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -18,18 +27,25 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json()
 
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    })
   }
 
-  return (<GithubContext.Provider value={{
-      users,
-      loading,
-      fetchUsers,
-  }}>
+  const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
       {children}
-  </GithubContext.Provider>)
+    </GithubContext.Provider>
+  )
 }
 
 export default GithubContext
-
